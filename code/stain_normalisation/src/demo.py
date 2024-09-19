5c7aa171edb91906ecbdd22ff381c928fe44d265
Nicholas Trahearn <30726374+ntrahearn@users.noreply.github.com>
2024-09-19 16:45:15 +0100

    Corrected tile paths

--- code/stain_normalisation/src/demo.py
+++ code/stain_normalisation/src/demo.py
@@ -1,10 +1,10 @@
 from norm_cws import norm_cws_by_stats_batch
 
-tile_path='../../tiles'
-mask_path='../../masks'
-out_path='../../norm'
+tile_path='../../../tiles'
+mask_path='../../../masks'
+out_path='../../../norm'
 
-target_path='../../ref/tiles'
-target_mask_path='../../ref/masks'
+target_path='../../../ref/tiles'
+target_mask_path='../../../ref/masks'
 
 norm_cws_by_stats_batch(tile_path, target_path, out_path, mask_path, target_mask_path, file_pattern='*.jpg')
